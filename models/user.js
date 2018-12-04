e095746aa080098e02640c46adeb7648ac16efe3
Timothy <superfluous11@gmail.com>
2018-12-04 12:05:56 -0800

    minor updates to user model

--- models/user.js
+++ models/user.js
@@ -19,8 +19,8 @@ class User extends Model {
 
       properties: {
         id: { type: 'integer' },
-        username: { type: 'string', minLength: 5, maxLength: 20 },
-        password: { type: 'string', minLength: 10, maxLength: 100 }
+        username: { type: 'string', minLength: 5 },
+        password: { type: 'string', minLength: 10 }
       }
     };
   }
