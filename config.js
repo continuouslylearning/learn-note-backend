f47022a75a9871cdea19d24c74abe4be1a5db31a
Morgan Newman <mpnewman95@gmail.com>
2018-12-05 07:14:54 -0800

    Adds defaults to env vars

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_SECRET: process.env.JWT_SECRET || 'not_a_real_password',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DB_URI: process.env.DB_URI
+  DB_URI: process.env.DB_URI || 'postgresql://dev:@localhost/learn-note'
 };
