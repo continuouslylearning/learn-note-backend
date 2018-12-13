79ad02e727191c3be6e92fc9c0814c9c43879d7d
Janet Leon <leon.irenejanet@gmail.com>
2018-12-13 12:31:07 -0800

    changes validation to check number

--- routes/validation/resource.js
+++ routes/validation/resource.js
@@ -1,63 +1,65 @@
 const Topic = require('../../models/topic');
 
-function validateResource(req, res, next){
-
+function validateResource(req, res, next) {
   const userId = req.user.id;
   const { parent, title } = req.body;
 
-  if('title' in req.body){
-    if(typeof title !== 'string'){
+  if ('title' in req.body) {
+    if (typeof title !== 'string') {
       const err = new Error('Title must be a string');
       err.status = 422;
       return next(err);
     }
   }
 
-  if(req.method === 'POST' && !('type' in req.body)){
+  if (req.method === 'POST' && !('type' in req.body)) {
     const err = new Error('Type is required');
     err.status = 422;
     return next(err);
   }
 
-  if('type' in req.body && (req.body.type !== 'youtube' && req.body.type !== 'other')){
-    const err = new Error('Only allowed values for `type` are `youtube` and `other`');
+  if (
+    'type' in req.body &&
+    (req.body.type !== 'youtube' && req.body.type !== 'other')
+  ) {
+    const err = new Error(
+      'Only allowed values for `type` are `youtube` and `other`'
+    );
     err.status = 422;
     return next(err);
   }
 
-  if('type' in req.body && req.body.type === 'youtube'){
+  if ('type' in req.body && req.body.type === 'youtube') {
     try {
       req.body.uri = req.body.uri.split('v=')[1].slice(0, 11);
-    }
-    catch(e){
+    } catch (e) {
       const err = new Error('Youtube url is invalid');
       err.status = 422;
       return next(err);
     }
   }
 
-  if(req.method === 'PUT' && !('parent' in req.body)) {
+  if (req.method === 'PUT' && !('parent' in req.body)) {
     return next();
   }
 
-  if(!parent){
+  if (!parent) {
     const err = new Error('Parent id is required');
     err.status = 400;
     return next(err);
   }
 
-  if(typeof parent !== 'number'){
+  if (typeof Number(parent) !== 'number') {
     const err = new Error('Parent must be an integer');
     err.status = 422;
     return next(err);
   }
 
-  Topic
-    .query()
+  Topic.query()
     .where({ userId, id: parent })
     .first()
     .then(topic => {
-      if(!topic){
+      if (!topic) {
         const err = new Error('Topic id is invalid');
         err.status = 422;
         return Promise.reject(err);
@@ -66,6 +68,5 @@ function validateResource(req, res, next){
     })
     .catch(next);
 }
-
 
 module.exports = validateResource;
