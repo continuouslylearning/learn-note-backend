f417e66b77311388828bee1caa4b28c1752df273
Timothy <superfluous11@gmail.com>
2018-12-19 17:37:39 -0800

    added duplicate title validation to put folders endpoint

--- routes/folders.js
+++ routes/folders.js
@@ -27,21 +27,37 @@ router.get('/', (req, res, next) => {
     .catch(next);
 });
 
-router.put('/:id', validateFolder, (req, res, next) => {
+router.put('/:id', validateFolder, async (req, res, next) => {
   const folderId = req.params.id;
   const userId = req.user.id;
   const { title } = req.body;
 
-  Folder.query()
-    .update({ title })
-    .where({ userId, id: folderId })
-    .returning('*')
-    .first()
-    .then(folder => {
-      if (!folder) return Promise.reject();
-      return res.status(201).json(folder);
-    })
-    .catch(next);
+  try {
+    const folderWithSameName = await Folder
+      .query()
+      .where({ userId, title })
+      .whereNot({ id: folderId })
+      .first();
+
+    if(folderWithSameName){
+      throw {
+        message: 'A folder with this title already exists',
+        status: 400
+      };
+    }
+    
+    const folder = await Folder
+      .query()
+      .update({ title })
+      .where({ userId, id: folderId })
+      .returning('*')
+      .first();
+
+    if(!folder) return next();
+    return res.status(201).json(folder);
+  } catch(e){
+    next(e);
+  }
 });
 
 router.post('/', requiredFields(['title']), validateFolder, (req, res, next) => {
@@ -67,20 +83,22 @@ router.post('/', requiredFields(['title']), validateFolder, (req, res, next) => 
     .catch(next);
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', async (req, res, next) => {
   const userId = req.user.id;
   const folderId = req.params.id;
 
-  Folder.query()
-    .delete()
-    .where({ userId, id: folderId })
-    .returning('*')
-    .first()
-    .then(folder => {
-      if (!folder) return Promise.reject();
-      return res.sendStatus(204);
-    })
-    .catch(next);
+  try {
+    const folder = await Folder.query()
+      .delete()
+      .where({ userId, id: folderId })
+      .returning('*')
+      .first();
+
+    if(!folder) return next();
+    return res.sendStatus(204);
+  } catch(e){
+    next(e);
+  }
 });
 
 module.exports = router;
