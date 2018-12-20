9f220d65123625b3476646b1afe521831f7ce17c
Timothy <superfluous11@gmail.com>
2018-12-20 03:20:37 -0800

    updates resources endpoints with aync/await syntax

--- models/resource.js
+++ models/resource.js
@@ -2,9 +2,29 @@ const { Model } = require('objection');
 
 class Resource extends Model {
 
-
   static get tableName(){
     return 'resources';
+  }
+
+  normalize(){
+    const normalized = {
+      id: this.id,
+      title: this.title,
+      uri: this.uri,
+      type: this.type,
+      completed: this.completed,
+      lastOpened: this.lastOpened,
+      parent: null
+    };
+
+    if(this.parentId && this.parentTitle){
+      normalized.parent = {
+        id: this.parentId,
+        title: this.parentTitle
+      };
+    }
+
+    return normalized;
   }
 }
 
